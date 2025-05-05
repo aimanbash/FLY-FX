bbdb49da0f56d34322f21fc2f4dbefe2a1d540d5
aimanbash <127540062+aimanbash@users.noreply.github.com>
2025-05-05 20:23:06 +0300

    Delete flyfx directory

--- flyfx/assets/js/nav-slide.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("mobileMenu");
-  const hamburger = document.getElementById("hamburgerIcon"); // Add an ID to your hamburger
-  menu.classList.toggle("active");
-  hamburger.classList.toggle("hidden"); // Toggle hidden or replace with 'open' class for icon change
-}
